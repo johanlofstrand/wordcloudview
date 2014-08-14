6757eab4f1c39dd0efbec5c666f3b209f7d2d000
Johan Löfstrand <johan.lofstrand@svt.se>
2014-08-14 17:18:54 +0200

    first version but with some encoding problems

--- web.js
+++ web.js
@@ -1,9 +1,56 @@
 
-var express = require('express');
-var app = express();
-app.get('/', function(request, response) {
-response.send('Lets kick it!');
+
+var express = require('express'),
+logger = require('logger'),
+http = require('http'),
+json = require('json');
+
+var bodyParser = require('body-parser')
+
+var app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use('/public', express.static(__dirname + '/public'));
+
+
+app.get('/', function(req, res) {
+		retrieveWordCounts(function(ready) {
+		res.render("index.ejs",{wordcounts:totalData});
+	});
 });
+
 var port = process.env.PORT || 5001;
 app.listen(port, function() {
 });
+
+var options = {
+  hostname: 'localhost',
+  port: 8080,
+  path: '/',
+  method: 'GET'
+};
+
+var totalData="";
+var retrieveWordCounts = function (ready) {
+	totalData="";
+	var req = http.request(options, function(res) {
+	  res.setEncoding('utf8');
+	  res.on('data', function (chunk) {
+	    totalData+=chunk;
+	  });
+	  res.on('end', function () {
+	    ready();
+	  });
+	});
+	req.on('error', function(e) {
+	  console.log('retrieveSonar: problem with request: ' + e.message);
+	});
+
+	req.end();
+
+}
